9a0ac2cc64c3bf748087405abf78916d1fda187e
Nina Vens <nina.vens@hva.nl>
2024-01-22 13:50:35 +0100

    added: styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,25 @@
 </script>
 
 <h1>Tweede kamer door de jaren heen</h1>
-<h2>In hoeverre hebben historische gebeurtenissen in Nederland invloed gehad op het stemgedrag van de bevolking tijdens de Tweede Kamer verkiezingen door de jaren heen?</h2>
-<!-- <Barchart /> -->
-<Chart />
-<!-- <Other /> -->
+<h2>
+    In hoeverre hebben historische gebeurtenissen in Nederland invloed gehad op
+    het stemgedrag van de bevolking tijdens de Tweede Kamer verkiezingen door de
+    jaren heen?
+</h2>
 
+<div class="chart-container">
+    <!-- <Barchart /> -->
+    <Chart />
+    <!-- <Other /> -->
+</div>
+
+<style>
+    .chart-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        margin-top: 2rem;
+    }
+</style>
