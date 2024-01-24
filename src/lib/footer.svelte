cd9041e275c38042b7d903cdc69fb779df1666cb
Nina Vens <nina.vens@hva.nl>
2024-01-24 19:26:40 +0100

    added: footer

---
+++ src/lib/footer.svelte
@@ -0,0 +1,30 @@
+<footer>
+    <p>
+        <strong>Disclaimer:</strong> Deze 'links/rechts' indeling is gebaseerd op standpunten
+        van de betrokken partijen en dient uitsluitend ter informatieve doeleinden.
+        Hoewel wij ons best doen om accurate en actuele informatie te verstrekken,
+        snappen wij dat niet iedereen dezelfde mening deelt. Onze beoordeling is
+        gebaseerd op zorgvuldig onderzoek van diverse bronnen, en deze bronnen zijn
+        op aanvraag beschikbaar voor verdere verificatie. De inhoud van deze indeling
+        vormt geen juridisch, professioneel of enig ander advies. Wij zijn niet verantwoordelijk
+        voor eventuele gevolgen die voortvloeien uit het gebruik van deze informatie.
+    </p>
+</footer>
+
+<style>
+    footer {
+        position: relative;
+        bottom: 0;
+        width: 100%;
+        margin: 1rem 0;
+        background-color: var(--bg-color);
+    }
+
+    footer p {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 1rem;
+        font-size: 0.65rem;
+        text-align: center;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script>
     import '../app.css';
+    import Footer from '$lib/footer.svelte';
 </script>
 
 <main>
     <slot />
 </main>
+
+<Footer />
+
+<style>
+    main {
+        min-height: 1800px;
+    }
+</style>
